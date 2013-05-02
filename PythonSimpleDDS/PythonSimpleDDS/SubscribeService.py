e90edc006281dbab77d952d0b725dd75b83edade
Paul Weng <wengjjpaul@gmail.com>
2013-05-02 10:29:09 +0800

    Update SubscribeService.py

--- PythonSimpleDDS/PythonSimpleDDS/SubscribeService.py
+++ PythonSimpleDDS/PythonSimpleDDS/SubscribeService.py
@@ -6,13 +6,13 @@ class SubscribeService:
         self.host = aHostIPV4
         self.port = aHostPort
         self.mDDSFilter = aDDSFilter
-    def testFilter(self, what):
+    def _testFilter(self, what):
         print(self.mDDSFilter.getSubscribers(what))
     def hostSubscribeService(self):
         self.mServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.mServer.bind((self.host, self.port))
-        self.startListening()
-    def startListening(self):
+        self._startListening()
+    def _startListening(self):
         while 1:
             message, address = self.mServer.recvfrom(8192)
             messageSendFromClient = message.decode("utf-8")
