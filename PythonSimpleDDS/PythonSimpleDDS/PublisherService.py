4ae3c98ceb4ee13511de4994f08d8b89664f3460
Scino <wengjjpaul@gmail.com>
2013-05-03 18:16:34 +0800

    123

--- PythonSimpleDDS/PythonSimpleDDS/PublisherService.py
+++ PythonSimpleDDS/PythonSimpleDDS/PublisherService.py
@@ -7,11 +7,13 @@ class PublisherService:
         self.port = aHostPort
         self.mDDSFilter = aDDSFilter
     def HostPublisherService(self):
+        #Setup listening socket to hear from publishers
         self.mServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.mServer.bind(('', self.port))
         self._startListening()
     def _startListening(self):
         while 1:
+            #Incoming messages stored in message
             message, address = self.mServer.recvfrom(8192)
             messageSendFromClient = message.decode("utf-8")
             messageParts = messageSendFromClient.split(',')
@@ -20,13 +22,20 @@ class PublisherService:
             if(tCommand):
                 if(tCommand == "publish"):
                     if(tTopic):
-                        #remove command and topic from message
+                        #remove command and topic segment from message array
                         messageParts = messageParts[2:]
                         #Make a comma separated string from list
                         messageToSend = ",".join(messageParts)
+                        #Clean up the message little
                         messageToSend.strip()
+                        #Get subscriber list for current topic
                         tSubscriberListForThisTopic = self.mDDSFilter.getSubscribers(tTopic)
+<<<<<<< HEAD
                         t = threading.Thread(target=self._publish, args = (self.mServer, messageToSend, tSubscriberListForThisTopic))
+=======
+                        #Begin thread to publish to all subscribers
+                        t = threading.Thread(target=_publish, args = (self.mServer, messageToSend, tSubscriberListForThisTopic))
+>>>>>>> 76dff2824c2130a2b57b24ad469fea286f49c6bd
                         #wont keep the thread up if main thread die
                         t.daemon = True
                         try:
@@ -36,7 +45,10 @@ class PublisherService:
                 else:
                     pass
     def _publish(self, aServer, aMessage, aSubscriberList):
+        #Convert String message into bytes
         tMessageInBytes = bytes(aMessage, 'UTF-8')
+        #Check if there are any subscribers
         if(aSubscriberList):
             for tSubscriber in aSubscriberList:
+                #Send out the message using UDP using listening socket
                 aServer.sendto(tMessageInBytes, tSubscriber)
